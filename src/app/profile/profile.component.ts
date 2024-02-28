19d320322a4ba72c6b86703dcbf5f71f30fae272
Stanislav Pisotskyi <spisotskyi@gmail.com>
2024-02-28 09:20:11 +0200

    add tab query param

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import {AccountComponent} from "../account/account.component";
+import {Router, ActivatedRoute} from "@angular/router";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
-    AccountComponent
+    AccountComponent,
+    NgClass
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  activeTab: string;
+  navLinkClass: string = 'nav-link';
+  activeNavLinkClass: string = 'nav-link active';
+  tabPanelClass: string = 'tab-pane fade';
+  activeTabPanelClass: string = 'tab-pane fade show active';
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    const tabParam: string|null = this.route.snapshot.queryParamMap.get('tab');
+    this.activeTab = tabParam === null ? 'tasks' : tabParam;
+  }
+
+  selectTab(tab: string) {
+    this.activeTab = tab;
+
+    const queryParams = { tab };
+
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams,
+        queryParamsHandling: 'merge',
+      }
+    );
+  }
 }
