c5aa57ec37a710a2fb755242d0c75f44fc461cee
Stanislav Pisotskyi <spisotskyi@gmail.com>
2024-03-07 08:44:22 +0200

    action elements for comments

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,13 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {CommentNode} from "../interfaces/comment-node";
-import {MatIcon} from "@angular/material/icon";
 import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-comment',
   standalone: true,
   imports: [
-    MatIcon,
     NgIf
   ],
   templateUrl: './comment.component.html',
